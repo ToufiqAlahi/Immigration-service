7332d37fa9e66bf25ad42a5eef8039baa9a75b83
ToufiqAlahi <nipuntan01@gmail.com>
2022-04-18 21:01:08 +0600

    tweaked Services components

---
+++ butterfinger-immigration/src/components/PAGES/ServiceDetail/ServiceDetail.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import "./ServiceDetail.css";
+import thanks from "./../../../Images/thanks.png";
+
+const ServiceDetail = () => {
+  const { serviceId } = useParams();
+  return (
+    <div>
+      <img className="w-50 mt-4" src={thanks} alt="" />
+      <div className="text-center w-25 mx-auto">
+        <Link to="/checkout">
+          <button className="btn btn-success"> Proceed Checkout</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceDetail;
