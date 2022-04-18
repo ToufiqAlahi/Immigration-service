dad07265ced8d60f0fd2ebd4bb710d941f608fef
ToufiqAlahi <nipuntan01@gmail.com>
2022-04-18 21:00:09 +0600

    Added Auth systems

---
+++ butterfinger-immigration/src/components/PAGES/Login/Login/Login.js
@@ -0,0 +1,108 @@
+import React, { useRef } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import SocialLogin from "../SocialLogin/SocialLogin";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loading from "../../Home/Loading/Loading";
+import auth from '../../../../firebase.init';
+
+
+const Login = () => {
+    const emailRef = useRef("");
+    const passwordRef = useRef("");
+    const navigate = useNavigate();
+
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+
+    let errorElement;
+
+    const [signInWithEmailAndPassword, user, loading, error] =
+        useSignInWithEmailAndPassword(auth);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        signInWithEmailAndPassword(email, password);
+    };
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
+    if (error) {
+        errorElement = <p className="text-danger">Error: {error?.message}</p>;
+    }
+
+    if (loading) {
+        return <Loading></Loading>;
+    }
+
+    const navigateRegister = (event) => {
+        navigate("/register");
+    };
+
+    const resetPassword = () => {
+        toast("please enter email address");
+    };
+
+    return (
+        <div>
+            <h2 className="text-primary text-center mt-4">Login Please</h2>
+            <div className="w-50 mx-auto">
+                <br />
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control
+                            ref={emailRef}
+                            type="email"
+                            placeholder="Enter email"
+                            required
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Control
+                            ref={passwordRef}
+                            type="password"
+                            placeholder="Password"
+                            required
+                        />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Login
+                    </Button>
+                </Form>
+                {errorElement}
+                <p>
+                    New here?{" "}
+                    <Link
+                        to="/register"
+                        className="text-primary pe-auto text-decoration-none"
+                        onClick={navigateRegister}
+                    >
+                        Please Register
+                    </Link>
+                </p>
+                <p>
+                    Forgot password?{" "}
+                    <button
+                        className="btn btn-link text-decoration-none m-0"
+                        onClick={resetPassword}
+                    >
+                        Reset Password
+                    </button>
+                </p>
+
+                <SocialLogin></SocialLogin>
+                <ToastContainer />
+            </div>
+        </div>
+    );
+};
+
+export default Login;
